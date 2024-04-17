8f8fb66eb7fc3002dc7c0dba83f00650efaf440d
unknown <mydulcse62.niter@gmail.com>
2024-04-17 20:35:43 +0600

    firebase variable model aplied

--- src/Firebase/Firebase-config.js
+++ src/Firebase/Firebase-config.js
@@ -1,17 +1,15 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAP4B3UCXqnWUGz9JoSPJb5E5E0yLqSH0w",
-  authDomain: "homehaven-auth.firebaseapp.com",
-  projectId: "homehaven-auth",
-  storageBucket: "homehaven-auth.appspot.com",
-  messagingSenderId: "414616189676",
-  appId: "1:414616189676:web:cdeb7eb1b475082c81c3c9"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase

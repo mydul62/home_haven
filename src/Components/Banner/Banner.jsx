8e4c176c4a565ec31c283ad2e2c2d6c437c97948
unknown <mydulcse62.niter@gmail.com>
2024-04-17 22:17:53 +0600

    complete

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -37,7 +37,7 @@ const Banner = () => {
         navigation={true}
         modules={[Autoplay,Pagination, Navigation]}
         onAutoplayTimeLeft={onAutoplayTimeLeft}
-        className="mySwiper h-[80vh]"
+        className="mySwiper h-[100vh]"
       >
         <SwiperSlide>
           <img
